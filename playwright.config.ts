0009ea35d4af8d2e1b78e141a97138df51400c67
Jakob Norlin <jakob.stahl91@gmail.com>
2025-03-19 21:22:46 +0100

    Use more than 1 worker in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	workers: process.env.CI ? 1 : undefined,
 	timeout: 3 * 60 * 1000,
 
 	reporter: [["list"], ["html"]],
