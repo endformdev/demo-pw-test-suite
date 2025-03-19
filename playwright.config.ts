33f930644bfe18896580712fd388a30c4f64d6c4
Jakob Norlin <jakob.stahl91@gmail.com>
2025-03-19 14:51:46 +0100

    Increase test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
+	timeout: 3 * 60 * 1000,
+
 	reporter: "html",
 	use: {
 		trace: "on-first-retry",
